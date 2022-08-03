5a9905f28f56a4eb222219e8368d72a734280899
Pedro Barão <phbarao@gmail.com>
2022-08-03 16:40:16 -0300

    feat: add filter component

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {loadedFonts ? <SignIn /> : <Loading />}
+      {loadedFonts ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
